5fafeaa5398e27763758fc321a291f986eab497f
LENOVO <LENOVO@DESKTOP-Q8R5Q7U>
2021-10-09 11:06:28 +0200

    drejtek

---
+++ ICTarrmethods/src/Katrori/DrejtkendeshiT.java
@@ -0,0 +1,19 @@
+package Katrori;
+
+public class DrejtkendeshiT {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Drejtkendeshii d1 = new Drejtkendeshii(2.2, 3.3);
+		d1.setA(2.2);
+		d1.setB(3.3);
+		System.out.println("a=> "+d1.getA()+" b=> "+d1.getB()+" syprina=> "+d1.syprinaD());
+		System.out.println("a=> "+d1.getA()+" b=> "+d1.getB()+" syprina=> "+d1.perimetriD());
+		System.out.println("a=> "+d1.getA()+" b=> "+d1.getB()+" syprina=> "+d1.isSquare());
+
+
+		
+
+	}
+
+}
